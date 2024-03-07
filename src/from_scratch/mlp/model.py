ab7faac0f96b674c1bc7fee1e9b30d68ab32d1b9
Kolosov Ihor <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2024-03-08 01:23:13 +0200

    f

--- src/from_scratch/mlp/model.py
+++ src/from_scratch/mlp/model.py
@@ -1,6 +1,7 @@
+from logging import LogRecord
 import numpy as np, pandas as pd
 from sklearn.model_selection import train_test_split
-
+from pprint import pprint
 
 class ActivationFunction:
     def __init__(self, function:str) -> None:
@@ -32,9 +33,9 @@ class MLP:
         self.activation_function = ActivationFunction('sigmoid')
         self.weights = None
         self.loss_function = lambda y_pred, y_true: np.mean((y_true - y_pred) ** 2)
+        self.loss_function_df = lambda y_pred, y_true: 2*(y_pred - y_true)
 
-
-    def fit(self, X, y, hidden_layers:list=[105, 2]):
+    def fit(self, X, y, hidden_layers:list=[3, 2, 2]):
         """ Example: [[0, 0], [1, 0], [0, 1]], [[0], [1], [1]] """
         X, y= np.array(X), np.array(y)
         n_features = X.shape[1]
@@ -54,22 +55,42 @@ class MLP:
         return self
 
 
-    def backpropagation(self, X, y):
+    def backpropagation(self, X, y, lr=0.3):
         X = np.array(X)
         y = np.array(y)
 
-        predicted = self.predict(X)
-        loss = self.loss_function(y, predicted)
+        outputs = []
 
-        dL = y - predicted 
-        self.activation_function.df(predicted)
+        # forward pass 
+        prev_inputs = X
+        i = 1
+        for  layer in self.weights_hidden:
+            z = prev_inputs @ layer
+            a = self.activation_function(z)
+            outputs.append({"neurons":z, "activations":a, "layer": i - 1})
+            prev_inputs = a
+            i += 1
 
-        for weight, bias in list(zip(self.weights_hidden, self.biases_hidden))[::-1]:
-            # weight -= partial derrivative of loss function in respect of weights * learning rate  
-            # bias -= partial derrivative of loss function in respect of bias * learning rate 
-            pass
-        #print(outputs, loss)
+        z = prev_inputs @ self.weights_outputs
+        a = self.activation_function(z)
+        outputs.append({"neurons":z, "activations":a, "layer": i-1})
 
+        loss = self.loss_function(a, y)
+        dloss = self.loss_function_df(a, y)
+        
+        unpack_layer = lambda layer: (layer['layer'], layer['activations'], layer['neurons'])
+
+        df = np.dot([dloss],  self.activation_function.df(a))
+        wgrad = [df]
+        weights = self.weights_outputs
+        for layer in outputs[::-1][1:]:
+            idx, al, zl = unpack_layer(layer)
+            df = np.dot(self.activation_function.df(zl), np.sum(np.dot(df, weights)))
+            weights -= lr * np.dot(df, al)
+            weights = self.weights_hidden[idx].T
+            wgrad.append(df)
+        
+        return loss
 
     def predict(self, X):
         X = np.array(X)
@@ -92,4 +113,37 @@ def get_datasets(path="./data.csv"):
     y = df['label']
     return train_test_split(X, y, train_size=0.8)
 
-print(MLP().fit([[0, 0], [1, 0], [0, 1]], [[0], [1], [1]]).backpropagation([[1, 0]], [[1]]))
+
+xor_X = [
+    [0, 0], 
+    [0, 1],
+    [1, 0],
+    [1, 1]
+]
+xor_y = [
+    [0],
+    [1],
+    [1],
+    [0]
+]
+
+
+mlp = MLP()
+mlp.fit(xor_X, xor_y)
+
+for i in range(4000):
+    loss = 0
+    lr = 2e-3
+    for X, y in zip(xor_X, xor_y):
+        loss += mlp.backpropagation(X, y, lr)
+    lr *= 0.95
+    print(loss)
+
+print(mlp.predict(xor_X[0]))
+print(mlp.predict(xor_X[1]))
+print(mlp.predict(xor_X[2]))
+print(mlp.predict(xor_X[3]))
+#print(mlp.predict(i))
+
+
+#https://medium.com/@tiago.tmleite/neural-networks-multilayer-perceptron-and-the-backpropagation-algorithm-a5cd5b904fde#:~:text=The%20idea%20of%20the%20backpropagation,reaching%20the%20input%20layer%20ofhttps://medium.com/@tiago.tmleite/neural-networks-multilayer-perceptron-and-the-backpropagation-algorithm-a5cd5b904fde#:~:text=The%20idea%20of%20the%20backpropagation,reaching%20the%20input%20layer%20of
